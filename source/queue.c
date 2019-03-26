f0bbca099dbed541945f9fc5c73270d18372f0e5
Knut V Eriksen <knutve@stud.ntnu.no>
2019-03-26 15:24:31 +0100

    26.03.2019 15:24 - Knut

--- source/queue.c
+++ source/queue.c
@@ -10,9 +10,18 @@ int queue_matrix [N_FLOORS][N_BUTTONS] = {} ;
 
  //reset all values of queue matrix to 0
 void reset_queue_matrix(void) {
-  for (int i = 0; i < N_FLOORS; i++) {
-    for (int j = 0; j < N_BUTTONS; i++) {
-      queue_matrix[i][j] = 0;
+  for (int floor = 0; i < N_FLOORS; i++) {
+    for (int button = 0; j < N_BUTTONS; i++) {
+      queue_matrix[floor][button] = 0;
     }
   }
 }
+
+
+
+//check button signals 
+for (int floor = 0; i < N_FLOORS; i++) {
+  for (int button = 0; j < N_BUTTONS; i++) {
+    if elev_get_button_signal(button, floor) {
+      queue_matrix[floor][button] = 1;
+    }
--- source/stateMachine.h
+++ source/stateMachine.h
@@ -2,3 +2,7 @@
 
 
 typedef enum {INIT, IDLE, MOVING, STOP, ESTOP} state;
+
+
+void init(); //set current state to INIT
+void set_state(state s); //changes the state to s
