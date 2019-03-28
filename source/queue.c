432cb3d04ca1eabd48a62e949dc1a8c3ab3c4961
Knut V Eriksen <knutve@stud.ntnu.no>
2019-03-28 14:49:02 +0100

    28.03 14:48

--- source/queue.c
+++ source/queue.c
@@ -8,20 +8,31 @@
 int queue_matrix [N_FLOORS][N_BUTTONS] = {} ;
 
 
- //reset all values of queue matrix to 0
+ //reset all values of queue matrix to 0 and turns off lights
 void reset_queue_matrix(void) {
   for (int floor = 0; i < N_FLOORS; i++) {
     for (int button = 0; j < N_BUTTONS; i++) {
       queue_matrix[floor][button] = 0;
+      elev_set_button_lamp(button, floor, 0);
     }
   }
 }
 
-
-
-//check button signals 
-for (int floor = 0; i < N_FLOORS; i++) {
-  for (int button = 0; j < N_BUTTONS; i++) {
-    if elev_get_button_signal(button, floor) {
-      queue_matrix[floor][button] = 1;
+//reset orders at floor "floor" and turns off lights
+void reset_floor_orders (int floor){
+    for (int button = 0; j < N_BUTTONS; i++) {
+      queue_matrix[floor][button] = 0;
+        elev_set_button_lamp(button, floor, 0);
     }
+}
+
+
+//set orders and turns on lights
+void set_order(void) {
+  for (int floor = 0; i < N_FLOORS; i++) {
+    for (int button = 0; j < N_BUTTONS; i++) {
+      if elev_get_button_signal(button, floor) {
+        queue_matrix[floor][button] = 1;
+        elev_set_button_lamp(button, floor, 1);
+      }
+}
--- source/queue.h
+++ source/queue.h
@@ -1,4 +1,8 @@
 
 
 
-void reset_queue_matrix(void);   //reset all values of queue matrix to 0
+void reset_queue_matrix(void);   //reset all values of queue matrix to 0 and turns off light
+
+void reset_floor_orders (int floor); //reset orders at floor "floor" and turns off light
+
+void set_order(void); // set orders and turns on light
