b5525b666f4a74d3e3bbf0b676bb04bd44a6ce33
Knut V Eriksen <knutve@stud.ntnu.no>
2019-03-29 13:08:13 +0100

    29.03 13:07 - Knut

--- source/queue.c
+++ source/queue.c
@@ -10,8 +10,8 @@ int queue_matrix [N_FLOORS][N_BUTTONS] = {} ;
 
  //reset all values of queue matrix to 0 and turns off lights
 void reset_queue_matrix(void) {
-  for (int floor = 0; i < N_FLOORS; i++) {
-    for (int button = 0; j < N_BUTTONS; i++) {
+  for (int floor = 0; floor < N_FLOORS; floor++) {
+    for (int button = 0; button < N_BUTTONS; button++) {
       queue_matrix[floor][button] = 0;
       elev_set_button_lamp(button, floor, 0);
     }
@@ -19,8 +19,8 @@ void reset_queue_matrix(void) {
 }
 
 //reset orders at floor "floor" and turns off lights
-void reset_floor_orders (int floor){
-    for (int button = 0; j < N_BUTTONS; i++) {
+void reset_floor (int floor){
+    for (int button = 0; button < N_BUTTONS; button++) {
       queue_matrix[floor][button] = 0;
         elev_set_button_lamp(button, floor, 0);
     }
@@ -28,11 +28,29 @@ void reset_floor_orders (int floor){
 
 
 //set orders and turns on lights
-void set_order(void) {
-  for (int floor = 0; i < N_FLOORS; i++) {
-    for (int button = 0; j < N_BUTTONS; i++) {
+void update_matrix(void) {
+  for (int floor = 0; floor < N_FLOORS; floor++) {
+    for (int button = 0; button < N_BUTTONS; button++) {
       if elev_get_button_signal(button, floor) {
         queue_matrix[floor][button] = 1;
         elev_set_button_lamp(button, floor, 1);
       }
+    }
+  }
+}
+
+//updates current floor and turns corresponding light on
+void update_current_floor(int floor) {
+  current_floor = floor;
+  elev_set_floor_indicator(floor);
+}
+
+
+void print_que_matrix() {
+  for (int floor = 0; floor < N_FLOORS; floor++) {
+    for (int button = 0; button < N_BUTTONS < button ++) {
+      printf("%d  ", queue_matrix[floor][button] );
+    }
+    printf("\n");
+  }
 }
--- source/queue.h
+++ source/queue.h
@@ -1,8 +1,12 @@
 
-
+int current_floor; //current floor variable
 
 void reset_queue_matrix(void);   //reset all values of queue matrix to 0 and turns off light
 
-void reset_floor_orders (int floor); //reset orders at floor "floor" and turns off light
+void reset_floor (int floor); //reset orders at floor "floor" and turns off light
 
-void set_order(void); // set orders and turns on light
+void update_matrix(void); // set orders and turns on light
+
+void update_current_floor(int floor);  //updates current floor and turns corresponding light on
+
+void print_que_matrix(); //prints the queue_matrix
